f67cffb9c15e2da61c47c046da9f05c23c1b6028
Siddhesh Zagade <siddeshzagade7@gmail.com>
2022-07-02 15:09:09 +0530

    NEWW

--- upload/urls.py
+++ upload/urls.py
@@ -25,4 +25,7 @@ urlpatterns = [
     path('', include('csvs.urls',namespace='csvs')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+      urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    
